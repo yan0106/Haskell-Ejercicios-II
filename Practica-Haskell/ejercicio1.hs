72c748c270e32d827564f971b7c64d71c68c1d0e
Yandira Diaz <yandiradiaz@hotmail.com>
2026-04-02 16:52:02 -0300

    Agregar f collect

--- Practica-Haskell/ejercicio1.hs
+++ Practica-Haskell/ejercicio1.hs
@@ -5,3 +5,18 @@ borrarUltimo :: [a] -> [a]
 borrarUltimo [] = []
 borrarUltimo [x] = []
 borrarUltimo (x:xs) = x : borrarUltimo xs
+
+-- b) collect :: [(k,v)] -> toma una lista de pares (clave, valor) y asocia cada clave única con todos los valores con
+-- los que estaba apareada originalmente. Por ejemplo: collect
+
+collect :: Ord k => [(k,v)] -> [(k,[v])]
+collect [] = []
+collect [(a,b)] = [(a,[b])]
+collect ((x,y):xs) = insertar x y (collect xs)
+
+-- func. auxiliar
+insertar :: Ord k => k -> v -> [(k, [v])] -> [(k, [v])]
+insertar x y [] = [(x, [y])]
+insertar x y ((s, (lista)):resto)
+    | x == s = (s, y:lista) : resto
+    | x /= s = (s, lista) : insertar x y resto
