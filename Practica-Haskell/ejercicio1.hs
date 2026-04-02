6bf4126d832662938bc689a0be7e5ff580d61265
Yandira Diaz <yandiradiaz@hotmail.com>
2026-04-02 14:51:09 -0300

    Crear f borrarUltimo

--- Practica-Haskell/ejercicio1.hs
+++ Practica-Haskell/ejercicio1.hs
@@ -1,3 +1,7 @@
 -- 1. Definir las siguientes funciones en forma recursiva:
 -- a) borrarUltimo, que dada una lista borra el último elemento de la lista. No utilizar reverse, ni tail.
 
+borrarUltimo :: [a] -> [a]
+borrarUltimo [] = []
+borrarUltimo [x] = []
+borrarUltimo (x:xs) = x : borrarUltimo xs
