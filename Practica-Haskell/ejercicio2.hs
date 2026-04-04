cb3dfa94683f371e7a12bb1952fac424fd988081
Yandira Diaz <yandiradiaz@hotmail.com>
2026-04-04 19:27:39 -0300

    Agregar archivo ejercicio2.hs

---
+++ Practica-Haskell/ejercicio2.hs
@@ -0,0 +1,4 @@
+-- 2. Definir usando listas por comprensión las funciones:
+-- a) cambios :: [a] -> [Int], que dada una lista, devuelve la lista de los índices en que la lista cambia.
+-- Es decir, dada la lista S retorna la lista con los i tal que Si /= S(i+1)
+-- cambios [1,1,1,3,3,1,1] = [2,4]
