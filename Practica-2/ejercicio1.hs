cec08a09b4588b48a956ad5d3a24be85068f4200
Yandira Diaz <yandiradiaz@hotmail.com>
2026-04-15 14:18:16 -0300

    Agregar cambios

---
+++ Practica-2/ejercicio1.hs
@@ -0,0 +1,38 @@
+import System.Win32 (COORD(xPos))
+{- 1. El modelo de color RGB es un modelo aditivo que tiene al rojo, verde y azul como colores
+primarios. Cualquier otro color se expresa en términos de las proporciones de estos tres colores 
+que es necesario combinar en forma aditiva para obtenerlo. Dichas proporciones caracterizan a cada
+color de manera biunivoca, por lo que usualmente se utilizan estos valores como representacion de
+un color.
+Definir un tipo Color en este modelo y una función mezclar que permita obtener el promedio
+componente a componente entre dos colores.-}
+
+type Color = (Int, Int, Int) -- 0 -> 255 (256) 2^.. ?
+
+data Color2 = C (red::Int, green::Int, blue::Int) -- record
+
+data Color3 = Negro 
+            | Blanco 
+            | Gris Int 
+            | C Int Int Int deriving Show
+
+mezclar :: Color -> Color -> Color
+mezclar (r1,g1,b1) (r2,g2,b2) = (div (r1+r2) 2, div (g1+g2) 2, div (b1+b2) 2)
+
+-- 2.
+type Cursor = Int
+type Linea = (String, Cursor)
+
+vacia = ("",0)
+
+moverIzq (xs,0) = (xs,0)
+moverIzq (xs,n) = (xs, n-1)
+--"Hola"
+moverDer (xs,p) | p >= length xs = (xs,p)
+                | otherwise = (xs,p+1)
+
+insertar c (xs,p) = (ins p c xs, p+1)
+
+ins 0 c xs = c:xs
+ins n c (x:xs) = x : ins (n-1) c xs
+
--- Practica-Haskell/ejercicio2.hs
+++ Practica-Haskell/ejercicio2.hs
@@ -1,4 +1,14 @@
 -- 2. Definir usando listas por comprensión las funciones:
--- a) cambios :: [a] -> [Int], que dada una lista, devuelve la lista de los índices en que la lista cambia.
+-- a) cambios :: [a] -> [Int], que dada una lista, devuelve la lista de los índices en que la 
+-- lista cambia.
 -- Es decir, dada la lista S retorna la lista con los i tal que Si /= S(i+1)
 -- cambios [1,1,1,3,3,1,1] = [2,4]
+
+-- b) oblongoNumber :: [Int] que genera la lista de los números oblongos. Un número es oblongo si
+-- es el producto de dos naturales consecutivos. Por ejemplo, los números [2,6,12,20,...]
+
+oblongoNumber :: [Int]
+oblongoNumber = [(x*(x+1)) | x <- [1..10]]
+
+--c) abundantes :: [Integer] que es la lista de todos los números abundantes. Un número natural n
+-- se denomina abundante si es menor que la suma de sus divisores propios. Por ejemplo, 12 y 30 
