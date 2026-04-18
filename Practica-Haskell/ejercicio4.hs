ae0175a113a79e49932f6db1c2f754eb2617f4a8
Yandira Diaz <yandiradiaz@hotmail.com>
2026-04-18 19:40:58 -0300

    Agregar resolución 4.d) y e)

--- Practica-Haskell/ejercicio4.hs
+++ Practica-Haskell/ejercicio4.hs
@@ -11,21 +11,39 @@ foo2 :: (b -> c) -> (a -> b) -> a -> c  -- (firma: de afuera hacia adentro)
 
 {- Inferencia de tipos: 
 (de adentro hacia afuera)
-
 1. z :: a (tipo solo)
 2. y :: a -> b (función)
 3. x :: b -> c (función)
-
 -}
 
 -- c) foo3 x y z = x y z 
 
 {- Inferencia:
-
 1. y :: a
 2. z :: b
 3. x :: (a->b->c)
-
 -}
 
 foo3 :: (a->b->c) -> a -> b -> c
+
+-- d) foo4 x y z = x y : z
+
+{- Inferencia:
+1. y :: a
+2. z :: [b]
+3. x :: (a->b)
+4. resultado :: [b]
+-}
+
+foo4 :: (a->b) -> a -> [b] -> [b]
+
+-- e) foo5 x y z = x : y z
+
+{- Inferencia:
+1. x :: a
+2. y :: (b->[a])
+3. z :: b
+4. resultado :: [a]
+-}
+
+foo5 :: a -> (b -> [a]) -> b -> [a]
