ed8946dd13cde5f9e80e28a3be9fe4a5e2ffef2b
Yandira Diaz <yandiradiaz@hotmail.com>
2026-04-20 14:54:12 -0300

    Agregar la f eval

--- Practica-2/ejercicio4.hs
+++ Practica-2/ejercicio4.hs
@@ -1,7 +1,22 @@
 {- 4. Dado el siguiente tipo algebraico:
 
-data Aexp = Num Int j Prod Aexp Aexp j Div Aexp Aexp
+data Aexp = Num Int | Prod Aexp Aexp | Div Aexp Aexp
 
 a) Defina un evaluador eval :: Aexp -> Int. ¿Cómo maneja los errores de división por 0?
 b) Defina un evaluador seval :: Aexp -> Maybe Int. -}
 
+data Aexp = Num Int | Prod Aexp Aexp | Div Aexp Aexp
+
+eval :: Aexp -> Int
+eval (Num n)      = n
+eval (Prod a b) = eval a * eval b
+eval (Div a b)  = if (intB == 0) then error "No se puede dividir por 0" 
+                    else div intA intB
+                    where intB = (eval b)
+                          intA = (eval a)
+                
+-- aplico Pattern Matching para desarmar el paquete
+-- llamo de forma recursiva a (eval a) o (eval b) para que me devuelva n = Int
+-- manejo el error de dividir por 0 con un if 
+
+-- seval :: Aexp -> Maybe Int
