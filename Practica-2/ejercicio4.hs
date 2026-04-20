e53fbc671821d125d04f6b9578960368604fbeb1
Yandira Diaz <yandiradiaz@hotmail.com>
2026-04-20 14:26:51 -0300

    Agregar ejercicio4.hs

---
+++ Practica-2/ejercicio4.hs
@@ -0,0 +1,7 @@
+{- 4. Dado el siguiente tipo algebraico:
+
+data Aexp = Num Int j Prod Aexp Aexp j Div Aexp Aexp
+
+a) Defina un evaluador eval :: Aexp -> Int. ¿Cómo maneja los errores de división por 0?
+b) Defina un evaluador seval :: Aexp -> Maybe Int. -}
+
